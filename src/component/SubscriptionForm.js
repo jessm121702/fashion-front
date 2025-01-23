045694acf59edd77214f621f0748515a7011f767
techanzy <techanzy@gmail.com>
2025-01-23 16:40:53 +0500

    contact-page-remove-card-info

--- src/component/SubscriptionForm.js
+++ src/component/SubscriptionForm.js
@@ -99,65 +99,6 @@ const SubscriptionForm = () => {
             </label>
           </div>
 
-          {/* Card Information */}
-          <div className="space-y-4 mb-6">
-            <input
-              type="text"
-              placeholder="Card Number"
-              className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-            />
-            <div className="grid grid-cols-2 gap-4">
-              <input
-                type="text"
-                placeholder="Exp date"
-                className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-              />
-              <input
-                type="text"
-                placeholder="Security code"
-                className="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-pink-400"
-              />
-            </div>
-          </div>
-
-          {/* Address */}
-          <div className="space-y-4 mb-6">
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="w-full border border-[#000000] rounded-md p-2 focus:outline-none"
-            />
-            <input
-              type="text"
-              placeholder="Street Address"
-              className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-            />
-            <div className="grid grid-cols-2 gap-4">
-              <input
-                type="text"
-                placeholder="City"
-                className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-              />
-              <input
-                type="text"
-                placeholder="State"
-                className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <input
-                type="text"
-                placeholder="Zip Code"
-                className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-              />
-              <input
-                type="text"
-                placeholder="Country"
-                className="w-full border border-[#000000] rounded-md p-2 focus:outline-none "
-              />
-            </div>
-          </div>
-
           {/* Submit Button */}
           <div className="text-center">
             <button
